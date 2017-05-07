d632877f662132232f9af643edc223734dc3948a
ejoysutton <joy.ericka.sutton@gmail.com>
2017-05-07 11:52:47 -0400

    added fields to the devise edit view for additional fields

---
+++ app/controllers/api/user_controller.rb
@@ -0,0 +1,42 @@
+class UserController < ApplicationController
+  def show
+    @posts = @user.posts
+  end
+
+  def edit
+    @user = current_user
+  end
+
+  def update
+    @user = current_user
+    if @user.update(user_update_params)
+      redirect_to user_profile_path
+    else
+      render :edit
+    end
+  end
+
+  def index
+    @user = current_user
+    @users = User.all
+  end
+
+  def profile
+    @user = current_user
+    @posts = @user.posts
+  end
+
+
+  private
+  def user_params
+    @user = User.find(params[:user_id])
+  end
+
+  def user_update_params
+    params.require(:user)
+        .permit(:username, :email, :bio, :avatar_url, :location)
+  end
+
+
+
+end
