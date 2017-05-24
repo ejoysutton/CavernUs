c01d81f6f6d89cccfe81999c21c4b068cc010cf0
ejoysutton <joy.ericka.sutton@gmail.com>
2017-05-24 13:15:41 -0400

    fixes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,6 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
+// = require lodash/lodash
 //= require angular/angular
 //= require angular-simple-logger/dist/angular-simple-logger
 //= require angular-ui-router/release/angular-ui-router
