2104a1a1a7c96f39e159776117829f0f14194904
ejoysutton <joy.ericka.sutton@gmail.com>
2017-05-24 13:26:26 -0400

    fixes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,6 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require lodash/lodash
 //= require jquery
 //= require jquery_ujs
 //= require angular/angular
@@ -18,6 +17,7 @@
 //= require angular-ui-router/release/angular-ui-router
 //= require angular-google-maps/dist/angular-google-maps
 //= require bootstrap-sprockets
+//= require lodash/lodash
 //= require_self
 //= require_tree .
 
